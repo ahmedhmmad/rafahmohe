b084caa80acda15d23c2096129319fe299dd8064
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-06-11 12:41:16 +0300

    Modifiy admin search-plan-school.blade.php

--- resources/views/admin/search-plan-school.blade.php
+++ resources/views/admin/search-plan-school.blade.php
@@ -17,7 +17,33 @@
                             </select>
                         </div>
                         <div class="col-md-4">
-                            <button type="submit" class="btn btn-primary mt-4 px-lg-4" style="transform:translatex(0px) translatey(5px);" >بحث</button>
+                            <label for="month" class="form-label"><strong>الشهر</strong></label>
+                            <select class="form-select" name="month" id="month" aria-label="Default select example">
+                                @php
+                                    $currentMonth = date('n');
+                                    $months = [
+                                        1 => 'يناير',
+                                        2 => 'فبراير',
+                                        3 => 'مارس',
+                                        4 => 'أبريل',
+                                        5 => 'مايو',
+                                        6 => 'يونيو',
+                                        7 => 'يوليو',
+                                        8 => 'أغسطس',
+                                        9 => 'سبتمبر',
+                                        10 => 'أكتوبر',
+                                        11 => 'نوفمبر',
+                                        12 => 'ديسمبر',
+                                        // Add the remaining months here
+                                    ];
+                                @endphp
+                                @foreach($months as $key => $value)
+                                    <option value="{{ $key }}" {{ $key == $currentMonth ? 'selected' : '' }}>{{ $value }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <button type="submit" class="btn btn-primary mt-4 px-lg-4" style="transform:translatex(0px) translatey(5px);">بحث</button>
                         </div>
                     </div>
                 </form>
@@ -25,56 +51,6 @@
         </div>
 
         <!-- Blade template code -->
-{{--        @if(isset($visits))--}}
-{{--            <div class="container py-2">--}}
-{{--                <div class="card px-4">--}}
-{{--                    <h2 class="p-4">نتائج البحث</h2>--}}
-{{--                    @if($visits->isEmpty())--}}
-{{--                        <div class="p-4">لا توجد نتائج للبحث.</div>--}}
-{{--                    @else--}}
-{{--                        <table class="table">--}}
-{{--                            <thead>--}}
-{{--                            <tr>--}}
-{{--                                <th scope="col">#</th>--}}
-{{--                                <th scope="col">اسم المدرسة</th>--}}
-{{--                                <th scope="col">تاريخ الزيارة</th>--}}
-{{--                                <th scope="col">الزائرون</th>--}}
-
-{{--                            </tr>--}}
-{{--                            </thead>--}}
-{{--                            <tbody>--}}
-{{--                            @foreach($visits as $index => $visit)--}}
-{{--                                <tr>--}}
-{{--                                    <th scope="row">{{ $index + 1 }}</th>--}}
-{{--                                    <td>{{ $visit->schools->name }}</td>--}}
-{{--                                    <td>{{ $visit->start }}</td>--}}
-{{--                                    <td>{{ $visit->user->name}}--}}
-{{--                                    </td>--}}
-
-{{--                                </tr>--}}
-{{--                            @endforeach--}}
-{{--                                @foreach($visits as $index => $visit)--}}
-{{--                                    @if ($index === 0 || $visit->start !== $visits[$index - 1]->start)--}}
-{{--                                        <tr>--}}
-{{--                                            <th scope="row">{{ $index + 1 }}</th>--}}
-{{--                                            <td>{{ $visit->schools->name }}</td>--}}
-{{--                                            <td>{{ $visit->start }}</td>--}}
-{{--                                            <td>{{ $visit->user->name }}</td>--}}
-{{--                                        </tr>--}}
-{{--                                    @else--}}
-
-{{--                                        <td>{{ $visit->user->name }},</td>--}}
-{{--                                    @endif--}}
-{{--                                @endforeach--}}
-
-
-{{--                            </tbody>--}}
-{{--                        </table>--}}
-{{--                    @endif--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        @endif--}}
-
         @if(isset($visits))
             <div class="container py-2">
                 <span class="card px-2">
@@ -106,13 +82,12 @@
                                             <td>{{ $visit->schools->name }}</td>
                                             <td>{{ $currentDate }}</td>
                                             <td>{{ $userNames }}</td>
-{{--                                            <td>{{ $visitorCount }}</td>--}}
                                             <td>
-                                            @if ($visitorCount >= 3)
+                                                @if ($visitorCount >= 3)
                                                     <span class="badge bg-danger p-2">{{ $visitorCount }}</span>
-                                            @else
-                                                {{ $visitorCount }}
-                                            @endif
+                                                @else
+                                                    {{ $visitorCount }}
+                                                @endif
                                             </td>
                                         </tr>
                                     @endif
@@ -135,17 +110,13 @@
                                     <td>{{ $currentDate }}</td>
                                     <td>{{ $userNames }}</td>
                                     <td>{{ $visitorCount }}</td>
-
                                 </tr>
                             @endif
                             </tbody>
                         </table>
-                    @endif
-                </div>
+                @endif
             </div>
-        @endif
-
-
-
+    </div>
+    @endif
     </div>
 @endsection
