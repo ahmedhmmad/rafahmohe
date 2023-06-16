5ca21a8fa571ded77f78c6f62c15e38202595929
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-06-17 00:56:41 +0300

    Start Api Process -head,admin phase 1

---
+++ app/Models/Ticket.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function assignedUser()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+}
