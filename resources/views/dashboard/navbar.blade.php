0c6cbde7cc3cdf4af521c15dd73118e2e078e079
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-07-14 11:20:00 +0300

    Events

--- resources/views/dashboard/navbar.blade.php
+++ resources/views/dashboard/navbar.blade.php
@@ -1,7 +1,4 @@
-<nav
-    class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
-    id="layout-navbar"
->
+<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
     <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
         <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
             <i class="bx bx-menu bx-sm"></i>
@@ -12,8 +9,7 @@
         <!-- Search -->
         <div class="navbar-nav-right d-flex align-items-center">
             <div class="nav-item d-flex align-items-center">
-
-               <label class="form-label w-100 mb-0">
+                <label class="form-label w-100 mb-0">
                     مديرية التربية والتعليم رفح
                 </label>
             </div>
@@ -21,7 +17,17 @@
         <!-- /Search -->
 
         <ul class="navbar-nav flex-row align-items-center mr-auto">
-             <!-- User -->
+            <!-- Notifications -->
+            <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <a class="nav-link dropdown-toggle hide-arrow" href="#" data-bs-toggle="dropdown">
+                    <i class="bx bx-bell"></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" id="notification-dropdown" data-user-id="{{ auth()->user()->id }}">
+                    <!-- Notification items will be added dynamically here -->
+                </ul>
+            </li>
+            <!--/ Notifications -->
+            <!-- User -->
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
@@ -40,7 +46,6 @@
                                 <div class="flex-grow-1">
                                     <span class="fw-semibold d-block">{{\Illuminate\Support\Facades\Auth::user()->name}}</span>
                                     <small class="text-muted">{{\Illuminate\Support\Facades\Auth::user()->department->name}}</small>
-
                                 </div>
                             </div>
                         </a>
@@ -60,7 +65,6 @@
                             <span class="align-middle">الاعدادت</span>
                         </a>
                     </li>
-
                     <li>
                         <div class="dropdown-divider"></div>
                     </li>
@@ -73,14 +77,8 @@
                 </ul>
             </li>
             <!--/ User -->
+
+
         </ul>
     </div>
 </nav>
-@if(session('success'))
-    <div class="toast align-items-center text-white bg-success border-0 show" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-body">
-            {{ session('success') }}
-        </div>
-        <button type="button" class="btn-close ms-auto me-2" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-@endif
