91292512c5dcc7f66ff8d7436555c1db963c28ea
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-08-04 20:07:43 +0300

    Modify Notifications

--- resources/views/dashboard/navbar.blade.php
+++ resources/views/dashboard/navbar.blade.php
@@ -23,62 +23,62 @@
 
         <ul class="navbar-nav flex-row align-items-center mr-auto">
             <!-- Notifications -->
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle hide-arrow" href="#" id="notificationsDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="bx bxs-bell-ring"></i>
-                    @if(count(\Illuminate\Support\Facades\Auth::user()->unreadNotifications) > 0)
-                        <span class="badge bg-label-danger">{{ count(\Illuminate\Support\Facades\Auth::user()->unreadNotifications) }}</span>
-                    @endif
-                </a>
-                @if(count(\Illuminate\Support\Facades\Auth::user()->unreadNotifications) > 0)
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="notificationsDropdown">
+{{--            <li class="nav-item dropdown">--}}
+{{--                <a class="nav-link dropdown-toggle hide-arrow" href="#" id="notificationsDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                    <i class="bx bxs-bell-ring"></i>--}}
+{{--                    @if(count(\Illuminate\Support\Facades\Auth::user()->unreadNotifications) > 0)--}}
+{{--                        <span class="badge bg-label-danger">{{ count(\Illuminate\Support\Facades\Auth::user()->unreadNotifications) }}</span>--}}
+{{--                    @endif--}}
+{{--                </a>--}}
+{{--                @if(count(\Illuminate\Support\Facades\Auth::user()->unreadNotifications) > 0)--}}
+{{--                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="notificationsDropdown">--}}
 
-                        <!-- Your existing notification loop -->
-                        @foreach(\Illuminate\Support\Facades\Auth::user()->unreadNotifications as $notification)
-                            @php
-                                $data = $notification->data['data'];
-                            @endphp
-                            <a class="dropdown-item" href="{{ $data['ticketId'] }}">
-                                <div class="d-flex">
-                                    <div class="flex-shrink-0 me-3">
-                                        <div class="avatar avatar-online">
-                                            <img src="{{ $data['ticketAttachment'] }}" alt class="w-px-40 h-auto rounded-circle" />
-                                        </div>
-                                    </div>
-                                    <div class="flex-grow-1">
-                                        <span class="fw-semibold d-block">{{ $data['ticketSchoolName'] }}</span>
-                                        <small class="text-muted">{{ $data['ticketSubject'] }}</small>
-                                    </div>
+{{--                        <!-- Your existing notification loop -->--}}
+{{--                        @foreach(\Illuminate\Support\Facades\Auth::user()->unreadNotifications as $notification)--}}
+{{--                            @php--}}
+{{--                                $data = $notification->data['data'];--}}
+{{--                            @endphp--}}
+{{--                            <a class="dropdown-item" href="{{ $data['ticketId'] }}">--}}
+{{--                                <div class="d-flex">--}}
+{{--                                    <div class="flex-shrink-0 me-3">--}}
+{{--                                        <div class="avatar avatar-online">--}}
+{{--                                            <img src="{{ $data['ticketAttachment'] }}" alt class="w-px-40 h-auto rounded-circle" />--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="flex-grow-1">--}}
+{{--                                        <span class="fw-semibold d-block">{{ $data['ticketSchoolName'] }}</span>--}}
+{{--                                        <small class="text-muted">{{ $data['ticketSubject'] }}</small>--}}
+{{--                                    </div>--}}
 
-                                    <form action="{{ route('notifications.markAsRead', $notification->id) }}" method="POST">
-                                        @csrf
-                                        <button type="submit" class="dropdown-item" data-bs-toggle="tooltip"
-                                                title="تحديد كمقروء"
-                                                data-bs-offset="0,4"
-                                                data-bs-placement="top">
-                                            <i class="bx bx-check me-2"></i>
-                                            <span class="align-middle"></span>
-                                        </button>
-                                    </form>
-                                </div>
-                            </a>
-                        @endforeach
+{{--                                    <form action="{{ route('notifications.markAsRead', $notification->id) }}" method="POST">--}}
+{{--                                        @csrf--}}
+{{--                                        <button type="submit" class="dropdown-item" data-bs-toggle="tooltip"--}}
+{{--                                                title="تحديد كمقروء"--}}
+{{--                                                data-bs-offset="0,4"--}}
+{{--                                                data-bs-placement="top">--}}
+{{--                                            <i class="bx bx-check me-2"></i>--}}
+{{--                                            <span class="align-middle"></span>--}}
+{{--                                        </button>--}}
+{{--                                    </form>--}}
+{{--                                </div>--}}
+{{--                            </a>--}}
+{{--                        @endforeach--}}
 
-                        <form action="{{ route('notifications.markAllAsRead') }}" method="POST">
-                            @csrf
-                            <button type="submit" class="dropdown-item">
-                                <i class="bx bx-check me-2"></i>
-                                <span class="align-middle">
-                    <strong>
-                        تعليم الكل كمقروء
-                    </strong>
-                </span>
-                            </button>
-                        </form>
+{{--                        <form action="{{ route('notifications.markAllAsRead') }}" method="POST">--}}
+{{--                            @csrf--}}
+{{--                            <button type="submit" class="dropdown-item">--}}
+{{--                                <i class="bx bx-check me-2"></i>--}}
+{{--                                <span class="align-middle">--}}
+{{--                    <strong>--}}
+{{--                        تعليم الكل كمقروء--}}
+{{--                    </strong>--}}
+{{--                </span>--}}
+{{--                            </button>--}}
+{{--                        </form>--}}
 
-                    </div>
-                @endif
-            </li>
+{{--                    </div>--}}
+{{--                @endif--}}
+{{--            </li>--}}
             <!--/ Notifications -->
 
             <!-- User -->
