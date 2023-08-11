66f53e9b8a7231d7d1cc564d3206178737c1c0ab
ahmedhammad <ahmed.hammad@students.iugaza.edu.ps>
2023-08-11 23:27:17 +0300

    Excel

--- app/Models/SchoolVisit.php
+++ app/Models/SchoolVisit.php
@@ -29,5 +29,10 @@ class SchoolVisit extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
 
 }
