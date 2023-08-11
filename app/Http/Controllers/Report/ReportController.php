8b37e58140af0b39af372464be652cd668302d6b
ahmedhammad <ahmed.hammad@students.iugaza.edu.ps>
2023-08-11 23:15:41 +0300

    Excel

---
+++ app/Http/Controllers/Report/ReportController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Report;
+
+use App\Exports\SchoolVisitExport;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ReportController extends Controller
+{
+    public function exportExcel()
+    {
+        return Excel::download(new SchoolVisitExport(), 'custom-school-visits.xlsx');
+    }
+}
