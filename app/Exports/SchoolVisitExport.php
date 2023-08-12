1e0a52d44894e25aa6206f3e015279cea367a550
ahmedhammad <ahmed.hammad@students.iugaza.edu.ps>
2023-08-12 11:21:04 +0300

    Excel

--- app/Exports/SchoolVisitExport.php
+++ app/Exports/SchoolVisitExport.php
@@ -5,21 +5,13 @@ namespace App\Exports;
 use App\Models\SchoolVisit;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
-use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
-use PhpOffice\PhpSpreadsheet\Cell\StringValueBinder;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use Illuminate\Support\Facades\View as ViewFacade;
 
-
-class SchoolVisitExport extends StringValueBinder implements FromView, WithCustomValueBinder
+class SchoolVisitExport implements FromView
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-//    public function collection()
-//    {
-//        //
-//    }
-
     public function view(): View
     {
         $groupedData = SchoolVisit::with('user', 'department')
@@ -29,5 +21,68 @@ class SchoolVisitExport extends StringValueBinder implements FromView, WithCusto
         return ViewFacade::make('exports.custom_school_visits', [
             'groupedData' => $groupedData,
         ]);
+    }
+
+    public function downloadExcel()
+    {
+        $view = $this->view();
+        $groupedData = $view->getData()['groupedData'];
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Set right-to-left direction for the entire sheet
+        $spreadsheet->getActiveSheet()->setRightToLeft(true);
+
+        // Add custom header and logo (adjust coordinates as needed)
+        $logoPath = public_path('/img/logo.webp');
+        $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+        $drawing->setName('Logo')->setPath($logoPath)->setCoordinates('B1')->setWorksheet($sheet);
+
+        $sheet->setCellValue('F1', 'مديرية التربية والتعليم رفح');
+        $sheet->setCellValue('F2', 'مكتب المدير');
+        $sheet->getStyle('F1:F2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+        // Adjust column widths
+        $numberOfColumns = 8; // Change this to match the number of columns in your view
+        $columnIndex = 'A';
+
+        foreach (range(0, $numberOfColumns - 1) as $col) {
+            $sheet->getColumnDimension($columnIndex)->setAutoSize(true);
+            $columnIndex++;
+        }
+
+        // Fill in your data from $groupedData
+        $row = 5;
+        foreach ($groupedData as $departmentName => $visits) {
+            foreach ($visits as $visit) {
+                $sheet->setCellValue('A' . $row, $departmentName);
+                $sheet->setCellValue('B' . $row, $visit->user->name);
+                $sheet->setCellValue('C' . $row, $visit->visit_date);
+                $sheet->setCellValue('D' . $row, $visit->coming_time);
+                $sheet->setCellValue('E' . $row, $visit->leaving_time);
+                $sheet->setCellValue('F' . $row, $visit->job_title);
+                $sheet->setCellValue('G' . $row, $visit->purpose);
+                $sheet->setCellValue('H' . $row, $visit->activities);
+                $row++;
+            }
+        }
+
+
+    // Create a temporary file path
+        $tempFilePath = tempnam(sys_get_temp_dir(), 'exported_excel');
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save($tempFilePath);
+
+        // Set the appropriate headers for downloading the file
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment; filename="exported_excel.xlsx"');
+        header('Cache-Control: max-age=0');
+
+        // Read and output the temporary file
+        readfile($tempFilePath);
+
+        // Clean up the temporary file
+        unlink($tempFilePath);
     }
 }
--- resources/views/exports/custom_school_visits.blade.php
+++ resources/views/exports/custom_school_visits.blade.php
@@ -1,91 +1,63 @@
-<!DOCTYPE html>
-<html lang="ar" dir="rtl">
-<head>
-    <meta charset="UTF-8">
-    <title> مديرية التربية والتعليم رفح</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 5px;
-            text-align: center;
-        }
-        .center {
-            text-align: center;
-        }
-        .logo-cell {
-            width: 20%; /* Adjust the logo cell width */
-            text-align: right; /* Align the logo to the right */
-            border: none; /* Remove border from the logo cell */
-        }
-        .logo {
-            max-width: 100px;
-            max-height: 100px;
-            margin: 0 auto; /* Center the logo */
-        }
-        .borderless {
-            border: none; /* Remove border from the header table */
-        }
-    </style>
-</head>
-<body>
-<table class="borderless">
-    <tr>
-        <td colspan="2" class="center borderless">
-            <strong>مديرية التربية والتعليم رفح</strong>
-        </td>
-        <td colspan="2" class="center logo-cell borderless">
-            <img class="logo" src="{{ public_path('/img/logo.webp') }}" alt="Logo">
-        </td>
-        <td colspan="2" class="center borderless">
-            <strong>Directrate of Education -Rafah</strong>
-        </td>
-    </tr>
-    <tr>
-        <td colspan="2" class="center borderless">
-            <strong>مكتب المدير</strong>
-        </td>
-        <td colspan="2" class="center borderless"></td>
+<?php
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 
-        <td colspan="2" class="center borderless">
-            <strong>Director Office</strong>
-        </td>
-    </tr>
-</table>
+// Create a new Spreadsheet instance
+$spreadsheet = new Spreadsheet();
 
+// Set right-to-left direction for the entire sheet
+$spreadsheet->getActiveSheet()->setRightToLeft(true);
 
-<table>
-    <thead>
-    <tr>
-        <th>القسم</th>
-        <th>الزائر</th>
-        <th>تاريخ الزيارة</th>
-        <th>وقت الحضور</th>
-        <th>وقت المغادرة</th>
-        <th>المسمى الوظيفي</th>
-        <th>أهداف الزيارة</th>
-        <th>ما تم تنفيذه</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($groupedData as $departmentName => $visits)
-        @foreach($visits as $visit)
-            <tr>
-                <td>{{ $departmentName }}</td>
-                <td>{{ $visit->user->name }}</td>
-                <td>{{ $visit->visit_date }}</td>
-                <td>{{ $visit->coming_time }}</td>
-                <td>{{ $visit->leaving_time }}</td>
-                <td>{{$visit->job_title}}</td>
-                <td>{{$visit->purpose}}</td>
-                <td>{{$visit->activities}}</td>
-            </tr>
-        @endforeach
-    @endforeach
-    </tbody>
-</table>
-</body>
-</html>
+// Get the active sheet
+$sheet = $spreadsheet->getActiveSheet();
+
+// Set header section with logo and title
+$logoPath = public_path('/img/logo.webp');
+$sheet->mergeCells('B1:D2'); // Merge cells for the logo
+$logoDrawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+$logoDrawing->setName('Logo')->setPath($logoPath)->setCoordinates('B1')->setWorksheet($sheet);
+
+$sheet->setCellValue('F1', 'مديرية التربية والتعليم رفح');
+$sheet->setCellValue('F2', 'مكتب المدير');
+$sheet->getStyle('F1:F2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+// Add column headers
+$columnHeaders = ['القسم', 'الزائر', 'تاريخ الزيارة', 'وقت الحضور', 'وقت المغادرة', 'المسمى الوظيفي', 'أهداف الزيارة', 'ما تم تنفيذه'];
+$columnIndex = 1;
+foreach ($columnHeaders as $header) {
+    $cellCoordinate = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($columnIndex) . '4';
+    $sheet->setCellValue($cellCoordinate, $header);
+    $columnIndex++;
+}
+
+// ... (Fill in your data from $groupedData as you did before)
+$row = 5;
+foreach ($groupedData as $departmentName => $visits) {
+    foreach ($visits as $visit) {
+        $sheet->setCellValue('A' . $row, $departmentName);
+        $sheet->setCellValue('B' . $row, $visit->user->name);
+        $sheet->setCellValue('C' . $row, $visit->visit_date);
+        $sheet->setCellValue('D' . $row, $visit->coming_time);
+        $sheet->setCellValue('E' . $row, $visit->leaving_time);
+        $sheet->setCellValue('F' . $row, $visit->job_title);
+        $sheet->setCellValue('G' . $row, $visit->purpose);
+        $sheet->setCellValue('H' . $row, $visit->activities);
+        $row++;
+    }
+}
+// Adjust column widths
+$columnIndex = 'A';
+foreach ($columnHeaders as $header) {
+    $sheet->getColumnDimension($columnIndex)->setAutoSize(true);
+    $columnIndex++;
+}
+
+// Create a new Xlsx writer and save the spreadsheet to a file
+$writer = new Xlsx($spreadsheet);
+
+// Set the appropriate headers for downloading the file
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment; filename="exported_excel.xlsx"');
+header('Cache-Control: max-age=0');
+$writer->save('php://output');
