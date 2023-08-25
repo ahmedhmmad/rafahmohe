d1eb0749dc83f55383bfecaac900003bdf26e7e5
ahmedhammad <ahmed.hammad@students.iugaza.edu.ps>
2023-08-25 12:49:34 +0300

    dispaly school next to each other on showonly-plan.blade.php

--- resources/views/employee/monthly-plan.blade.php
+++ resources/views/employee/monthly-plan.blade.php
@@ -23,19 +23,17 @@
                                 <option value="10">اكتوبر</option>
                                 <option value="11">نوفمبر</option>
                                 <option value="12">ديسمبر</option>
-                                <!-- Add the remaining months as options -->
+
                             </select>
                         </div>
                         <div class="col-md-4">
                             <label for="year" class="form-label"><strong>السنة</strong></label>
-
                             <select class="form-select" name="year" id="year" aria-label="Default select example">
                                 <option selected>اختر السنة</option>
                                 <option value="2023">2023</option>
                                 <option value="2024">2024</option>
                                 <option value="2025">2025</option>
                             </select>
-
                         </div>
                         <div class="col-md-4">
                             <button type="submit" class="btn btn-primary mt-4 px-lg-4">عرض</button>
@@ -43,6 +41,62 @@
                     </div>
                 </form>
 
+{{--                <table class="table table-bordered table-hover">--}}
+{{--                    <thead class="table-primary">--}}
+{{--                    <tr>--}}
+{{--                        <th>اليوم</th>--}}
+{{--                        <th>التاريخ</th>--}}
+{{--                        <th>المدارس</th>--}}
+{{--                    </tr>--}}
+{{--                    </thead>--}}
+{{--                    <tbody>--}}
+{{--                    @php--}}
+{{--                        $previousDate = null;--}}
+{{--                        $currentDate = null;--}}
+{{--                        $schoolsString = '';--}}
+{{--                        $dayNames = ['الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت', 'الأحد'];--}}
+{{--                    @endphp--}}
+{{--                    @foreach ($plans as $plan)--}}
+{{--                        @php--}}
+{{--                            $currentDate = $plan->start;--}}
+{{--                        @endphp--}}
+
+{{--                        @if ($currentDate === $previousDate)--}}
+{{--                            @php--}}
+{{--                                $schoolsString .= ', ' . $plan->schools->name;--}}
+{{--                            @endphp--}}
+{{--                        @else--}}
+{{--                            @if ($previousDate !== null)--}}
+{{--                                <tr>--}}
+{{--                                    <td>{{ $dayNames[date('N', strtotime($previousDate)) - 1] }}</td>--}}
+{{--                                    <td>{{ $previousDate }}</td>--}}
+{{--                                    <td>{{ $schoolsString }}</td>--}}
+{{--                                </tr>--}}
+{{--                            @endif--}}
+
+{{--                            @php--}}
+{{--                                $schoolsString = $plan->schools->name;--}}
+{{--                            @endphp--}}
+{{--                        @endif--}}
+
+{{--                        @php--}}
+{{--                            $previousDate = $currentDate;--}}
+{{--                        @endphp--}}
+{{--                    @endforeach--}}
+
+{{--                    @if ($previousDate !== null)--}}
+{{--                        <tr>--}}
+{{--                            <td>{{ $dayNames[date('N', strtotime($previousDate)) - 1] }}</td>--}}
+{{--                            <td>{{ $previousDate }}</td>--}}
+{{--                            <td>{{ $schoolsString }}</td>--}}
+{{--                        </tr>--}}
+{{--                    @endif--}}
+{{--                    </tbody>--}}
+{{--                </table>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--@endsection--}}
                 <table class="table table-bordered table-hover">
                     <thead class="table-primary">
                     <tr>
@@ -53,52 +107,32 @@
                     </thead>
                     <tbody>
                     @php
-                        $previousDate = null;
-                        $currentDate = null;
                         $dayNames = ['الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت', 'الأحد'];
                     @endphp
-                    @foreach ($plans as $plan)
-                        @php
-                            $currentDate = $plan->start;
-                        @endphp
 
-                        {{-- Display a row for working days with no schools --}}
-                        @foreach ($workingDays as $workingDay)
-                            @if ($workingDay > $previousDate && $workingDay < $currentDate && date('N', strtotime($workingDay)) != 5 && date('N', strtotime($workingDay)) != 6)
-                                <tr>
-                                    <td>{{ $dayNames[date('N', strtotime($workingDay)) - 1] }}</td>
-                                    <td><strong>{{ $workingDay }}</strong></td>
-                                    <td>لا توجد مدارس</td>
-                                </tr>
-                            @endif
-                        @endforeach
-
-                        @if ($plan->schools)
-                            <tr>
-                                <td>{{ $dayNames[date('N', strtotime($plan->start)) - 1] }}</td>
-                                <td>
-                                    @if ($plan->start !== $previousDate)
-                                        <strong>{{ $plan->start }}</strong>
-                                    @endif
-                                </td>
-                                <td>{{ $plan->schools->name }}</td>
-                            </tr>
-                        @endif
-
-                        @php
-                            $previousDate = $currentDate;
-                        @endphp
-                    @endforeach
-
-                    {{-- Add a row for working days after the last plan --}}
                     @foreach ($workingDays as $workingDay)
-                        @if ($workingDay > $previousDate && date('N', strtotime($workingDay)) != 5 && date('N', strtotime($workingDay)) != 6)
-                            <tr>
-                                <td>{{ $dayNames[date('N', strtotime($workingDay)) - 1] }}</td>
-                                <td><strong>{{ $workingDay }}</strong></td>
-                                <td>لا توجد مدارس</td>
-                            </tr>
-                        @endif
+                        @php
+                            $schoolsString = '';
+
+                            // Find schools for the current working day
+                            $schoolsForDay = $plans->where('start', $workingDay)->pluck('schools.name')->toArray();
+
+                            if (!empty($schoolsForDay)) {
+                                $schoolsString = implode(', ', $schoolsForDay);
+                            }
+                        @endphp
+
+                        <tr>
+                            <td>{{ $dayNames[date('N', strtotime($workingDay)) - 1] }}</td>
+                            <td>{{ $workingDay }}</td>
+                            <td>
+                                @if ($schoolsString)
+                                    {{ $schoolsString }}
+                                @else
+                                    لا توجد مدارس
+                                @endif
+                            </td>
+                        </tr>
                     @endforeach
                     </tbody>
                 </table>
