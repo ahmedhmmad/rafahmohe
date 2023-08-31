9ba855cc4a3169b8043d40a3b4d93897b90b6d63
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-08-31 09:23:24 +0300

    Update MonthlyPlan.php

--- app/Http/Controllers/Employee/MonthlyPlan.php
+++ app/Http/Controllers/Employee/MonthlyPlan.php
@@ -232,8 +232,10 @@ class MonthlyPlan extends Controller
         $user = Auth::user();
 
         // Get the start and end dates of the recent month
-        $startOfMonth = now()->addMonth()->startOfMonth();
-        $endOfMonth = now()->addMonth()->endOfMonth();
+       // $startOfMonth = now()->addMonth()->startOfMonth();
+      //  $endOfMonth = now()->addMonth()->endOfMonth();
+        $startOfMonth = now()->startOfMonth()->addMonth();
+    $endOfMonth = now()->endOfMonth()->addMonth();
 
         // Retrieve the plans for the recent month and order them by date
         $plans = Plan::where('user_id', $user->id)
