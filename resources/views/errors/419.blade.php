034a25e380e86ae24093800c3906b972b57608ba
ahmedhammad <ahmed.hammad@students.iugaza.edu.ps>
2023-09-16 00:19:46 +0300

    404 and 419 error pages

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>419 Error - Page Expired</title>
+    <style>
+        /* Add your custom CSS styles here */
+        body {
+            background-color: #f7f7f7;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 100px;
+        }
+        h1 {
+            font-size: 36px;
+        }
+        p {
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+<h1>419 Error - Page Expired</h1>
+<p>Sorry, the page has expired due to inactivity. Please refresh the page and try again.</p>
+</body>
+</html>
