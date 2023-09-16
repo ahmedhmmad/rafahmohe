f3cc32e0098a98fd9f885a7fc8a2bed123565661
ahmedhammad <ahmed.hammad@students.iugaza.edu.ps>
2023-09-16 22:12:08 +0300

    debug

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -66,6 +66,8 @@ public function timeline()
         // Retrieve the data for the table (e.g., plan restrictions) with paginatiob
         $planRestrictions = PlanRestriction::paginate(10);
 
+        dd($planRestrictions);
+
 
         // Pass the data to the view
         return view('admin.table-override-data', compact('planRestrictions'));
