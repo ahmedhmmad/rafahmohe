0aa2f7c3428663295170321fe95849ae243fff4d
ahmedhammad <ahmed.hammad@students.iugaza.edu.ps>
2023-09-17 11:23:29 +0300

    Add Pagination to assigned tickets for users

--- resources/views/employee/show-assigned-tickets.blade.php
+++ resources/views/employee/show-assigned-tickets.blade.php
@@ -65,6 +65,9 @@
                         @endforeach
                         </tbody>
                     </table>
+                    <div class="d-flex justify-content-center mt-3">
+                        {{ $tickets->links() }}
+                    </div>
                 @endif
             </div>
         </div>
