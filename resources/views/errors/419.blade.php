ff6861f4ecf54770381c3a92b140b527aab0de1e
ahmedhammad <ahmed.hammad@students.iugaza.edu.ps>
2023-09-30 23:27:53 +0300

    Update 419 Error Page

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -1,27 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ar">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>419 Error - Page Expired</title>
+    <title>خطأ 419 - انتهت صلاحية الصفحة</title>
     <style>
-        /* Add your custom CSS styles here */
+        /* Custom CSS styles */
         body {
-            background-color: #f7f7f7;
-            font-family: Arial, sans-serif;
+            background-color: #282c36; /* Darker shade for contrast */
+            font-family: 'Tajawal', Arial, sans-serif; /* Used a font that looks better for Arabic */
             text-align: center;
             padding: 100px;
+            color: #ffffff; /* White text for better contrast on dark background */
         }
         h1 {
-            font-size: 36px;
+            font-size: 48px; /* Increased font size for better visibility */
+            margin-bottom: 20px;
         }
         p {
-            font-size: 18px;
+            font-size: 24px; /* Made the paragraph text a bit bigger */
+            margin-bottom: 30px;
+        }
+        button {
+            background-color: #e74c3c; /* A shade of red */
+            color: #ffffff; /* White text on the button */
+            border: none;
+            padding: 10px 20px;
+            font-size: 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        button:hover {
+            background-color: #c0392b; /* Darker shade of red on hover */
         }
     </style>
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
-<h1>419 Error - Page Expired</h1>
-<p>Sorry, the page has expired due to inactivity. Please refresh the page and try again.</p>
+<h1>خطأ 419 - انتهت صلاحية الصفحة</h1>
+<p>عذراً، انتهت صلاحية الصفحة. يرجى تحديث الصفحة والمحاولة مرة أخرى.</p>
+<button onclick="location.href='/logout';">تسجيل الخروج</button> <!-- Logout button with a dummy logout link, replace with your actual logout link -->
 </body>
 </html>
